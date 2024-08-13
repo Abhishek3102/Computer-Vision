13d0371eef58c060210831fdb516f3d67b51d146
Abhishe16 <ankushpan694@gmail.com>
2024-08-13 13:17:28 +0530

    made changes

--- HandTrackingProject/first.py
+++ HandTrackingProject/first.py
@@ -3,10 +3,8 @@ import cv2
 import mediapipe as mp
 import time
 
-# Suppress TensorFlow warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-# Initialize the MediaPipe hands module
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 mp_draw = mp.solutions.drawing_utils
@@ -14,10 +12,8 @@ mp_draw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
-# Initialize the camera
 cap = cv2.VideoCapture(0)
 
-# Check if the camera opened successfully
 if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
