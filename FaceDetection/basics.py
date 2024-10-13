5b0d75ea977c48dfa3ca1c1d2beef17022a22e3c
Abhishe16 <ankushpan694@gmail.com>
2024-10-13 22:49:30 +0530

    added face detection code

---
+++ FaceDetection/basics.py
@@ -0,0 +1,48 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture("messi_zizou.mp4")
+pTime = 0
+
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+faceDetection = mpFaceDetection.FaceDetection(0.75)
+
+while True:
+    success, img = cap.read()
+
+    if not success:
+        print("Error reading frame")
+        break
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceDetection.process(imgRGB)
+    print(results)
+
+    if results.detections:
+        for id, detection in enumerate(results.detections):
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, ic = img.shape
+            # Calculate top-left and bottom-right corners of the bounding box
+            x1, y1 = int(bboxC.xmin * iw), int(bboxC.ymin * ih)
+            x2, y2 = int((bboxC.xmin + bboxC.width) * iw), int((bboxC.ymin + bboxC.height) * ih)
+
+            # Draw the rectangle with the calculated coordinates
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 2)
+            cv2.putText(img, f'{int(detection.score[0] * 100)}%',
+                        (x1, y1 - 20), cv2.FONT_HERSHEY_PLAIN,
+                        2, (255, 0, 255), 2)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (0, 255, 0), 2)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
